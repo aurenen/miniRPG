2b184a25e0bf598de86a3b76952a9fbb05d70bef
aurenen <aurenen@users.noreply.github.com>
2015-04-07 12:37:57 -0700

    Created base battle page

--- header.php
+++ header.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $site_title; ?></title>
     <link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic' rel='stylesheet' type='text/css'>
-    <link href="css/style.css" rel="stylesheet">
     <link href="css/flatly/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
