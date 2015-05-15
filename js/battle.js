504569825992089f778851b1d221acae221dd91a
aurenen <aurenen@users.noreply.github.com>
2015-05-15 16:28:37 -0700

    Fixed status bar %

--- js/battle.js
+++ js/battle.js
@@ -5,7 +5,7 @@ function spcost(points)
   // else
   //   alert("You ran out of SP!");
   document.getElementById('playerSP').innerHTML = myCurrentSP;
-  document.getElementById('playerSPbar').style.width = myCurrentSP * 10 + "%";
+  document.getElementById('playerSPbar').style.width = myCurrentSP / myTotalSP * 100 + "%";
 }
 
 function enemy()
@@ -15,7 +15,7 @@ function enemy()
     // myCurrentHP -= 10;
     myCurrentHP -= rnd;
   document.getElementById('playerHP').innerHTML = enemyCurrentHP;
-  document.getElementById('playerHPbar').style.width= enemyCurrentHP + "%";
+  document.getElementById('playerHPbar').style.width= enemyCurrentHP / enemyTotalHP * 100 + "%";
 
   document.getElementById('battle-text').innerHTML = "<b>You</b> are hit for " + rnd + " hp";
   document.getElementById('battle-text').style.background = "Tomato";
@@ -30,7 +30,7 @@ function attack() {
     spcost(1);
   }
   document.getElementById('monsterHP').innerHTML = enemyCurrentHP;
-  document.getElementById('monsterHPbar').style.width= enemyCurrentHP + "%";
+  document.getElementById('monsterHPbar').style.width= enemyCurrentHP / myTotalHP * 100 + "%";
 
   document.getElementById('battle-text').innerHTML = "<b>Enemy</b> hit for " + rnd + " hp";
   document.getElementById('battle-text').style.background = "SpringGreen";
